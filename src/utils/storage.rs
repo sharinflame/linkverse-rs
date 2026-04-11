91edc8bf7febc1ea0af9e6717e8c2acf08b254b9
Danik <retikadni@gmail.com>
2026-04-11 22:00:31 +0200

    Allow dead code

--- src/utils/storage.rs
+++ src/utils/storage.rs
@@ -11,6 +11,7 @@ type HmacSha256 = Hmac<Sha256>;
 pub const PUBLIC_PATH: &str = "https://storage.sharinflame.com";
 
 #[derive(Debug)]
+#[allow(dead_code)]
 pub enum Operation {
     PUT,
     DELETE,
